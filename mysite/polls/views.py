0edc7d79fd1eca1e0e0bca041a9a0b26d45a49a4
obada <142669183+obadax@users.noreply.github.com>
2025-07-24 19:21:19 +0300

    s

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world. 725d339f is the polls index.")
